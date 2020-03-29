da73efcde0fbfd00d95731e2649593adb340e5c8
Srikant Karnani <srikant.karnani352@gmail.com>
2020-03-29 13:14:31 +0530

    Update 6th.kt
    When using kotlin try to use the functions provided by it to reduce lines of code

--- Exercise 1/6th.kt
+++ Exercise 1/6th.kt
@@ -23,3 +23,15 @@
             println("'$c'" + " doesn't have a pair")
     }
 }
+
+// fun main() {
+//     var s1 = "abccdcirrbc"
+//     val filter = s1.toCharArray().toSet().filter { c -> s1.count { it == c } % 2 == 1 }
+//     filter.forEach { println("$it is alone")  }
+// }
+
+// fun main() {
+//     var s1 = "abccdcirrbc"
+//     val filter = s1.chars().filter { c -> s1.count { it.toInt() == c } % 2 == 1 }
+//     filter.forEach { println(it.toChar() + " is alone")  }
+// }
