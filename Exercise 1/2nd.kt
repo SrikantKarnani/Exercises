6772139911cfacd766c2e7ef636c2271e50d8c22
Srikant Karnani <srikant.karnani352@gmail.com>
2020-03-29 14:56:37 +0530

    Update 2nd.kt

--- Exercise 1/2nd.kt
+++ Exercise 1/2nd.kt
@@ -6,6 +6,8 @@ fun main(args : Array<String>) {
     var s1 = "hello this is kotlin language . kotlin language is new java language"
     var arr = s1.split(" ")
 
+    //    arr.groupingBy { it }.eachCount().forEach { t, u -> if (u > 1) println("$t repeats $u times") }
+
     val map = HashMap<String, Int>()
 
     for(i in arr){
